52e51e99cf8b197758672daa5191266a77ba0d3f
Maycon40 <71737549+Maycon40@users.noreply.github.com>
2025-10-13 18:28:11 -0300

    Comentando websocket

--- server.js
+++ server.js
@@ -8,21 +8,21 @@ dotenv.config();
 const PORT = process.env.PORT_EXPRESS || 3000;
 const port = process.env.PORT || 8080;
 
-const wss = new WebSocketServer({ port });
+//const wss = new WebSocketServer({ port });
 
-wss.on("connection", (ws) => {
-  console.log(`Novo usuário conectado`);
+//wss.on("connection", (ws) => {
+//  console.log(`Novo usuário conectado`);
 
-  ws.on("error", console.error);
+//  ws.on("error", console.error);
 
-  ws.on("message", (data) => {
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === client.OPEN) {
-        client.send(data);
-      }
-    });
-  });
-});
+//  ws.on("message", (data) => {
+//    wss.clients.forEach((client) => {
+//      if (client !== ws && client.readyState === client.OPEN) {
+//        client.send(data);
+//      }
+//    });
+//  });
+//});
 
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
